944d001781f0e64d60b43f461ae35df81392174a
KongLimheng <banakama12@gmail.com>
2025-04-21 13:44:08 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
+
+import { askEmailStep, onDocument } from './text';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { development, production } from './core';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const ENVIRONMENT = process.env.NODE_ENV || '';
+
+const bot = new Telegraf(BOT_TOKEN);
+
+const downloadDir = path.join(process.cwd(), 'downloads');
+const userStates = new Map();
+
+// Check and create downloads directory if not exist
+if (!fs.existsSync(downloadDir)) {
+  fs.mkdirSync(downloadDir);
+}
+
+bot.start(async (ctx) => {
+  userStates.clear();
+  userStates.set(ctx.chat.id, { step: 'askEmail' });
+
+  await ctx.sendChatAction('typing');
+  ctx.reply(
+    `Hello 🤚, ${ctx.message.from.first_name} ${ctx.message.from.last_name}! \n\nPlease enter your email address ✉️...`,
+  );
+});
+
+// bot.command('about', about());
+bot.on(message('text'), async (ctx) => await askEmailStep({ ctx, userStates }));
+bot.on(
+  message('document'),
+  async (ctx) =>
+    await onDocument({ ctx, userStates, downloadPath: downloadDir }),
+);
+
+//prod mode (Vercel)
+export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
+  await production(req, res, bot);
+};
+//dev mode
+ENVIRONMENT !== 'production' && development(bot);
---
+++ src/text/message.ts
@@ -0,0 +1,145 @@
+import { Context, NarrowedContext } from 'telegraf';
+import createDebug from 'debug';
+import { Message, Update } from 'telegraf/typings/core/types/typegram';
+import * as path from 'path';
+import * as fs from 'fs';
+import axios from 'axios';
+import { createTransport } from 'nodemailer';
+
+const debug = createDebug('bot:text:message');
+
+type TextMessageContext = NarrowedContext<
+  Context<Update>,
+  Update.MessageUpdate<Message.TextMessage>
+>;
+
+type DocumentMessageContext = NarrowedContext<
+  Context<Update>,
+  Update.MessageUpdate<Message.DocumentMessage>
+>;
+
+type BotContext = {
+  ctx: TextMessageContext;
+  userStates: Map<any, any>;
+};
+
+type BotDocContext = {
+  ctx: DocumentMessageContext;
+  userStates: Map<any, any>;
+  downloadPath: string;
+};
+
+const replyToMessage = (ctx: Context, messageId: number, string: string) =>
+  ctx.reply(string, {
+    reply_parameters: { message_id: messageId },
+  });
+
+export const askEmailStep = async ({ ctx, userStates }: BotContext) => {
+  const state = userStates.get(ctx.chat?.id);
+
+  if (!state) {
+    debug('No state found for user, sending /start command.');
+    ctx.deleteMessage(ctx.message.message_id);
+    return ctx.reply('Please type /start to begin.');
+  }
+
+  const message = ctx.message;
+  if (state.step === 'askEmail') {
+    const email = message.text.trim();
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+    if (!isValidEmail) {
+      ctx.deleteMessage(message.message_id);
+      return ctx.reply(
+        '❌ That doesn’t look like a valid email address. Please enter a valid email (e.g. user@example.com).',
+      );
+    }
+
+    state.email = email;
+
+    state.step = 'waitForAttachment';
+    await ctx.reply('Please upload your file 📄(.csv, .xls, .xlsx only).');
+  } else {
+    ctx.reply('Please send a file or type /start to begin again.');
+  }
+};
+
+export const onDocument = async ({
+  ctx,
+  userStates,
+  downloadPath,
+}: BotDocContext) => {
+  const state = userStates.get(ctx.chat.id);
+  if (!state) return ctx.reply('Please type /start to begin.');
+  if (!state || state.step !== 'waitForAttachment') {
+    return ctx.reply('Please follow the process using /start.');
+  }
+
+  const file = ctx.message.document;
+  const fileName = file.file_name?.toLowerCase() || '';
+
+  // Validate file type
+  const allowedExts = ['.csv', '.xls', '.xlsx'];
+  const isValid = allowedExts.some((ext) => fileName?.endsWith(ext));
+
+  if (!isValid) {
+    ctx.deleteMessage(ctx.message.message_id);
+    return ctx.reply(
+      '❌ Invalid file type. Only 📄 .csv, .xls, or .xlsx are allowed.',
+    );
+  }
+
+  try {
+    await ctx.sendChatAction('typing');
+    const waitText = await ctx.reply('please wait...');
+
+    const fileLink = await ctx.telegram.getFileLink(file.file_id);
+    const filePath = path.join(downloadPath, fileName);
+    const writer = fs.createWriteStream(filePath);
+
+    const response = await axios({
+      url: fileLink.href,
+      method: 'GET',
+      responseType: 'stream',
+    });
+
+    response.data.pipe(writer);
+
+    writer.on('finish', async () => {
+      await sendEmail(state.email, filePath, fileName);
+
+      ctx.deleteMessage(waitText.message_id);
+      ctx.reply('✅ Your information and file have been sent via email!');
+      fs.unlinkSync(filePath); // Cleanup
+      console.log('=====> state', userStates);
+      userStates.delete(ctx.chat.id);
+    });
+
+    writer.on('error', (err) => {
+      ctx.reply('⚠️ Failed to process the file.');
+      console.error(err);
+    });
+  } catch (err) {
+    ctx.reply('⚠️ Error downloading the file.');
+    console.error(err);
+  }
+};
+
+async function sendEmail(email: string, filePath: string, fileName: string) {
+  const transporter = createTransport({
+    service: 'gmail',
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"Telegram Bot" <${process.env.SMTP_USER}>`,
+    to: 'brosheng140@gmail.com',
+    subject: '📎 New File Submission from Telegram Bot',
+    text: `Email: ${email}`,
+    attachments: [{ filename: fileName, path: filePath }],
+  });
+}
